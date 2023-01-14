b17844de5ad73b4789f70b3ecb8067891cb0b100
cynthiaxhua <cynthiaxhua@gmail.com>
2023-01-14 12:33:21 -0500

    december data updates

--- content_interactions/content_interactions.py
+++ content_interactions/content_interactions.py
@@ -6,10 +6,11 @@ import matplotlib.font_manager
 import numpy as np
 
 #---READ IN DATA--
-df = pd.read_csv('../data/corrected_metrics.csv')
+df = pd.read_csv('../data/reader_metrics.tsv', sep='\t')
+corrected_df = pd.read_csv('../data/corrected_metrics.csv')
 
 #display top rows for preview
-#df.head()
+#df.iloc[0,:] 
 
 #---CLEAN DATA--
 #print out data types
@@ -18,26 +19,34 @@ df = pd.read_csv('../data/corrected_metrics.csv')
 #print(df.interactions_corrected.dtype)
 
 #remove commas
-df["total_pageview"] = df["total_pageview"].str.replace(",","")
-df["interactions"] = df["interactions"].str.replace(",","")
-df["interactions_corrected"] = df["interactions_corrected"].str.replace(",","")
+corrected_df["interactions"] = corrected_df["interactions"].str.replace(",","")
+corrected_df["interactions_corrected"] = corrected_df["interactions_corrected"].str.replace(",","")
 
 #convert string to datetime
 df['month'] = pd.to_datetime(df['month'])
+corrected_df['month'] = pd.to_datetime(corrected_df['month'])
 
 #truncate to preferred date range
-df = df[df["month"].isin(pd.date_range("2018-05-01", "2022-10-01"))]
+df = df[df["month"].isin(pd.date_range("2018-05-01", "2022-12-01"))]
+corrected_df = corrected_df[corrected_df["month"].isin(pd.date_range("2018-05-01", "2022-12-01"))]
 
 #convert to int
-df['interactions'] = df['interactions'].astype(str).astype(int)
-df['interactions_corrected'] = df['interactions_corrected'].astype(str).astype(int)
+corrected_df['interactions'] = corrected_df['interactions'].astype(str).astype(float)
+corrected_df['interactions_corrected'] = corrected_df['interactions_corrected'].astype(str).astype(float)
+
+#combine datasets — add corrected values to the reader metrics dataset
+df['interactions_corrected'] = df['interactions']
+correction_range = pd.date_range(start='2021-05-01', end='2022-02-01', freq='MS')
+for m in correction_range:
+	row_index = df[df['month'] == m].index 
+	correct_row = corrected_df.loc[corrected_df['month'] ==  m]
+	df.loc[row_index, 'interactions_corrected'] = correct_row['interactions_corrected']
 
 #---BREAK DATA INTO SUBSETS--
 #create subsets of data for easier plotting
-#before_data_loss = df[df["month"].isin(pd.date_range("2018-05-01", "2021-05-01"))]
-#after_data_loss = df[df["month"].isin(pd.date_range("2022-02-01", "2022-10-01"))]
 data_loss_df = df[df["month"].isin(pd.date_range("2021-05-01", "2022-02-01"))]
-october_df = df[df['month'].dt.month == 10]
+#subset of just data for month of interest
+monthly_df = df[df['month'].dt.month == 12]
 #subset to highlight the last two months
 yoy_highlight = pd.concat([df.iloc[-13,:],df.iloc[-1,:]],axis=1).T
 #subset to highlight specific months (manually entered)
@@ -46,8 +55,6 @@ yoy_highlight = pd.concat([df.iloc[-13,:],df.iloc[-1,:]],axis=1).T
 #---PREPARE TO PLOT ---
 #adjust plot size
 plt.rcParams["figure.figsize"] = [10, 6]
-#fig, ax = plt.subplots()
-#plt.figure(figsize=(10, 6))
 
 #create a dictionary for colors
 wmf_colors = {'black75':'#404040','black50':'#7F7F7F','black25':'#BFBFBF','base80':'#eaecf0','base70':'#c8ccd1','blue':'#0E65C0','brightblue':'#049DFF','brightbluelight':'#C0E6FF','yellow':'#F0BC00','green':'#308557','brightgreen':'#71D1B3'}
@@ -65,7 +72,7 @@ plt.grid(axis = 'y', color = wmf_colors['black25'], linewidth = 0.25)
 
 #---PLOT---
 #plot data loss area
-plt.fill_between(data_loss_df.month, data_loss_df.interactions, data_loss_df.interactions_corrected,
+plt.fill_between(df.month, df.interactions, df.interactions_corrected,
 	label='Data Loss Correction',
 	color=wmf_colors['base80'],
 	edgecolor=wmf_colors['base80'],
@@ -79,7 +86,7 @@ plt.plot(df.month, df.interactions_corrected,
 	zorder=4)
 
 #draw circle on octobers by plotting scatter
-plt.scatter(october_df.month, october_df.interactions_corrected,
+plt.scatter(monthly_df.month, monthly_df.interactions_corrected,
 	label='_nolegend_',
 	color=wmf_colors['blue'],
 	zorder=5)
@@ -97,23 +104,24 @@ plt.scatter(yoy_highlight.month, yoy_highlight.interactions_corrected,
 
 
 #---FORMATTING---
-#add title and labels
+#add title and axis labels
 plt.title('Content Interactions',font='Montserrat',weight='bold',fontsize=24,loc='left')
 #plt.xlabel("Month",font='Montserrat', fontsize=18, labelpad=10) #source serif pro
 #plt.ylabel("Active Editors",font='Montserrat', fontsize=14)
 
 #add october x-axis labels
 date_labels = []
-for dl in october_df['month']:
+for dl in monthly_df['month']:
 	date_labels.append(datetime.datetime.strftime(dl, '%b %Y'))
+
+#adjust axis ticks
+plt.yticks(fontsize=14)
 #add major ticks
 #plt.rcParams["xtick.major.size"] = 20
-plt.xticks(ticks=october_df['month'],labels=date_labels,fontsize=14,minor=False)
+plt.xticks(ticks=monthly_df['month'],labels=date_labels,fontsize=14,minor=False)
 #add minor ticks
 #plt.rcParams["xtick.minor.size"] = 2
 #plt.xticks(ticks=df['month'],minor=True)
-plt.yticks(fontsize=14)
-
 #print(plt.rcParams["xtick.major.size"])
 
 #format axis labels
@@ -144,11 +152,15 @@ for pos in ['right', 'top', 'bottom', 'left']:
 #YoY Change Annotation
 #calculate YoY change
 yoy_change_percent = ((yoy_highlight['interactions_corrected'].iat[-1] - yoy_highlight['interactions_corrected'].iat[0]) /  yoy_highlight['interactions_corrected'].iat[0]) * 100
-#make YoY annotation
-yoy_annotation = f"+{yoy_change_percent:.1f}% YoY largely due to rise in automated traffic"
+#make YoY annotation (add text her if relevant)
+if yoy_change_percent > 0:
+	yoy_annotation = f"+{yoy_change_percent:.1f}% YoY"
+else:
+	yoy_annotation = f"{yoy_change_percent:.1f}% YoY" 
+#annotate
 plt.annotate(yoy_annotation,
 	xy = (yoy_highlight['month'].iat[-1],yoy_highlight['interactions_corrected'].iat[-1]),
-	xytext = (-55,20),
+	xytext = (20,-5),
 	xycoords = 'data',
 	textcoords = 'offset points',
 	color='black',
@@ -163,7 +175,7 @@ plt.figtext(0.1, 0.02, "Graph Notes: Created by Hua Xi 12/12/22 using data from 
 
 #---SHOW GRAPH---
 #save as image
-plt.savefig('charts/ContentInteractions_3_light.png', dpi=300)
+plt.savefig('charts/Dec_1.png', dpi=300)
 #show in window
 plt.show()
 
