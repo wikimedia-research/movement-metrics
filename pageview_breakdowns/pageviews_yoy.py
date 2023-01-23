e3179e79de277b1f325617129872112a8c6f0f6b
cynthiaxhua <cynthiaxhua@gmail.com>
2023-01-23 16:41:06 -0500

    adjusted to feedback

--- pageview_breakdowns/pageviews_yoy.py
+++ pageview_breakdowns/pageviews_yoy.py
@@ -27,7 +27,7 @@ df.sort_values(by='timestamp')
 df = df[df["timestamp"].isin(pd.date_range("2022-02-01", "2022-12-01"))]
 
 #---ADJUST PLOT SIZE---
-plt.figure(figsize=(10, 6))
+plt.figure(figsize=(12, 6))
 
 #---PREPARE TO PLOT
 #create a dictionary for colors
@@ -58,20 +58,24 @@ plt.scatter(mom_highlight.timestamp, mom_highlight.sum_view_count,
 
 #---FORMATTING---
 #add title and labels
-plt.title('Monthly Pageviews 2022',font='Montserrat',weight='bold',fontsize=24,loc='left')
-#plt.xlabel("Month",font='Montserrat', fontsize=18, labelpad=10) #source serif pro
+plt.title('Monthly Automated Pageviews',font='Montserrat',weight='bold',fontsize=24,loc='left')
+plt.xlabel("2022",font='Montserrat', fontsize=18, labelpad=10) #source serif pro
 #plt.ylabel("Active Editors",font='Montserrat', fontsize=18)
 
 #expand bottom margin
-#plt.subplots_adjust(bottom=0.2, left=0.1, right=0.75)
+plt.subplots_adjust(bottom=0.15,left=0.1,right=0.825)
 
 #remove bounding box
 for pos in ['right', 'top', 'bottom', 'left']:
 	plt.gca().spines[pos].set_visible(False)
 
 #format y-axis labels
+def y_label_formatter(value):
+	formatted_value = '{:1.1f}B'.format(value*1e-9)
+	formatted_value = formatted_value.replace('.0','')
+	return formatted_value
 current_values = plt.gca().get_yticks()
-plt.gca().set_yticklabels(['{:1.1f}B'.format(x*1e-9) for x in current_values])
+plt.gca().set_yticklabels([y_label_formatter(x) for x in current_values])
 plt.yticks(fontname = 'Montserrat',fontsize=14)
 
 
@@ -84,13 +88,10 @@ plt.xticks(ticks=df['timestamp'],labels=date_labels,fontsize=10,fontname = 'Mont
 #---ADD ANNOTATIONS---
 #add combined annotation
 def annotate():
-	change_percent = ((df['sum_view_count'].iat[-1] - df['sum_view_count'].iat[-2]) /  df['sum_view_count'].iat[-2]) * 100
-	if change_percent > 0:
-		annotation = f" +{change_percent:.1f}% MoM" + " " + note
-	else:
-		annotation = f" {change_percent:.1f}% MoM" + " " + note
+	pageviews = df['sum_view_count'].iat[-1]
+	annotation = '{:1.2f}B Pageviews'.format(pageviews*1e-9)
 	plt.annotate(annotation,
-		xy = (df['timestamp'].iat[-1],df['sum_view_count'].iat[-1]),
+		xy = (df['timestamp'].iat[-1],pageviews),
 		xytext = (20,-5),
 		xycoords = 'data',
 		textcoords = 'offset points',
@@ -102,7 +103,7 @@ def annotate():
 annotate()
 
 #data notes
-plt.figtext(0.1, 0.025, "Graph Notes: Created by Hua Xi 12/12/22 using data from https://docs.google.com/spreadsheets/d/1Aw5kjj47cEi-PSX0eApCUp3Ww9_XNyARxcdoL9QnHp4", fontsize=8, family='Montserrat',color= wmf_colors['black25'])
+plt.figtext(0.1, 0.015, "Graph Notes: Created by Hua Xi 12/12/22 using data from https://docs.google.com/spreadsheets/d/1Aw5kjj47cEi-PSX0eApCUp3Ww9_XNyARxcdoL9QnHp4", fontsize=8, family='Montserrat',color= wmf_colors['black25'])
 
 #---SHOW GRAPH---
 save_file_name = "charts/" + outfile_name
