0d8cdfbb0433b94742b5117f70b80d7a7d9f4f57
cynthiaxhua <cynthiaxhua@gmail.com>
2023-02-19 22:08:42 -0500

    timeline updates, jan updates

--- net_new_content/net_new.py
+++ net_new_content/net_new.py
@@ -7,6 +7,8 @@ import matplotlib.font_manager
 import numpy as np
 from matplotlib.ticker import FormatStrFormatter
 import re
+import calendar
+from datetime import date
 
 #---PROMPT FOR INPUT---
 outfile_name = input('Outfile_name:\n') or "Net_New_Chart.png"
@@ -26,15 +28,20 @@ df = pd.read_csv('../data/editor_metrics.tsv', sep='\t')
 #print(df.net_new_Wikipedia_articles.dtype)
 #print(df.net_new_content_pages.dtype)
 
+start_date = "2018-05-01"
+end_date = "2023-01-01"
+month_interest = 1
+month_name = calendar.month_name[month_interest]
+
 #convert string to datetime
 df['month'] = pd.to_datetime(df['month'])
 
 #truncate to preferred date range
-df = df[df["month"].isin(pd.date_range("2018-05-01", "2022-12-01"))]
+df = df[df["month"].isin(pd.date_range(start_date, end_date))]
 
 #---BREAK DATA INTO SUBSETS--
 #create subsets of data for easier plotting
-monthly_df = df[df['month'].dt.month == 12]
+monthly_df = df[df['month'].dt.month == month_interest]
 #subset to highlight the last two months
 yoy_highlight = pd.concat([df.iloc[-13,:],df.iloc[-1,:]],axis=1).T
 #subset to highlight specific months (manually entered)
@@ -60,33 +67,40 @@ plt.grid(axis = 'y', color = wmf_colors['black25'], linewidth = 0.25)
 #dashed light black line
 #plt.grid(axis = 'y', color = wmf_colors['black25'], linestyle = '--', linewidth = 0.5)
 
+key = pd.DataFrame([['net_new_Commons_content_pages','Commons',wmf_colors['pink']],
+	['net_new_Wikidata_entities','Wikidata',wmf_colors['brightgreen']],
+	['net_new_Wikipedia_articles','Wikipedia',wmf_colors['purple']]],
+	index=['commons','wikidata','wikipedia'],
+	columns=['colname','labelname','color'])
+#key.loc['commons','colname']
+
 #---PLOT---
 #plot data
 plt.plot(df.month, df.net_new_Commons_content_pages,
-	label="Commons",
-	color=wmf_colors['pink'],
+	label=key.loc['commons','labelname'],
+	color=key.loc['commons','color'],
 	zorder=4)
 plt.plot(df.month, df.net_new_Wikidata_entities,
-	label="Wikidata",
-	color=wmf_colors['brightgreen'],
+	label=key.loc['wikidata','labelname'],
+	color=key.loc['wikidata','color'],
 	zorder=4)
 plt.plot(df.month, df.net_new_Wikipedia_articles,
-	label="Wikipedia",
-	color=wmf_colors['purple'],
+	label=key.loc['wikipedia','labelname'],
+	color=key.loc['wikipedia','color'],
 	zorder=4)
 
 #draw circle on octobers by plotting scatter
 plt.scatter(monthly_df.month, monthly_df.net_new_Commons_content_pages,
 	label='_nolegend_',
-	color=wmf_colors['pink'],
+	color=key.loc['commons','color'],
 	zorder=5)
 plt.scatter(monthly_df.month, monthly_df.net_new_Wikidata_entities,
 	label='_nolegend_',
-	color=wmf_colors['brightgreen'],
+	color=key.loc['wikidata','color'],
 	zorder=5)
 plt.scatter(monthly_df.month, monthly_df.net_new_Wikipedia_articles,
 	label='_nolegend_',
-	color=wmf_colors['purple'],
+	color=key.loc['wikipedia','color'],
 	zorder=5)
 
 #draw circle on 2021 and 2022 to highlight for comparison
@@ -104,25 +118,11 @@ plt.scatter(yoy_highlight.month, yoy_highlight.interactions_corrected,
 
 #---FORMATTING---
 #add title and labels
-plt.title('Net New Content',font='Montserrat',weight='bold',fontsize=24,loc='left')
+plt.title(f'Net New Content ({month_name})',font='Montserrat',weight='bold',fontsize=24,loc='left',pad=15)
 #plt.xlabel("Month",font='Montserrat', fontsize=18, labelpad=10) #source serif pro
 #plt.ylabel("Active Editors",font='Montserrat', fontsize=14)
 
-#add monthly x-axis labels
-date_labels = []
-for dl in monthly_df['month']:
-	date_labels.append(datetime.datetime.strftime(dl, '%b %Y'))
-#add major ticks
-#plt.rcParams["xtick.major.size"] = 20
-plt.xticks(ticks=monthly_df['month'],labels=date_labels,fontsize=14,minor=False)
-#add minor ticks
-#plt.rcParams["xtick.minor.size"] = 2
-#plt.xticks(ticks=df['month'],minor=True)
-plt.yticks(fontsize=14)
-
-#print(plt.rcParams["xtick.major.size"])
-
-#format axis labels
+#add y axis labels
 def y_label_formatter(value):
 	formatted_value = '{:1.1f}M'.format(value*1e-6)
 	#remove trailing zeros after decimal point only
@@ -131,7 +131,25 @@ def y_label_formatter(value):
 current_values = plt.gca().get_yticks()
 plt.gca().set_yticklabels([y_label_formatter(x) for x in current_values])
 plt.yticks(fontname = 'Montserrat',fontsize=14)
+
+#format x axis
 plt.xticks(fontname = 'Montserrat',fontsize=14)
+
+#add monthly x-axis labels
+'''
+date_labels = []
+for dl in monthly_df['month']:
+	date_labels.append(datetime.datetime.strftime(dl, '%b %Y'))
+plt.xticks(ticks=monthly_df['month'],labels=date_labels,fontsize=14,minor=False)
+'''
+
+#yearly x-axis labels on January
+date_labels = []
+date_labels_raw = pd.date_range(start_date, end_date, freq='AS-JAN')
+for dl in date_labels_raw:
+	date_labels.append(datetime.datetime.strftime(dl, '%Y'))
+plt.xticks(ticks=date_labels_raw,labels=date_labels)
+
 
 '''
 #add legend
@@ -154,23 +172,50 @@ for pos in ['right', 'top', 'bottom', 'left']:
 	plt.gca().spines[pos].set_visible(False)
 
 #---ADD ANNOTATIONS---
+#a = commons, b = wikidata, c = wikipedia
+#check to see if labels need to adjusted for overlap
+lastys = pd.DataFrame([yoy_highlight["net_new_Commons_content_pages"].iat[-1],
+	yoy_highlight["net_new_Wikidata_entities"].iat[-1],
+	yoy_highlight["net_new_Wikipedia_articles"].iat[-1]],
+	index=['commons','wikidata','wikipedia'],
+	columns=['lasty'])
+lastys = lastys.sort_values(by=['lasty'],ascending=True)
+#add column for y-padding
+#set padding to 0 for lowest entry
+lastys['ypad']=0
+#doubles padding in case both labels need to be moved up
+padmultiplier = 1 
+#set remaining two paddings
+for i in range(1,3):
+	valuedistance = lastys.iloc[i]['lasty'] - lastys.iloc[i-1]['lasty']
+	if valuedistance < 150000:
+		lastys.at[lastys.iloc[i].name,'ypad'] = 5 * padmultiplier
+	padmultiplier += 1
+
 #add legend as data labels
-def legend_annotate(data_label, legend_label, label_color):
+def legend_annotate(data_label, legend_label, label_color,ypad):
 	plt.annotate(legend_label,
 		xy = (df['month'].iat[-1],df[data_label].iat[-1]),
-		xytext = (20,-5),
+		xytext = (20,-5 + ypad),
 		xycoords = 'data',
 		textcoords = 'offset points',
 		color=label_color,
 		fontsize=14,
 		weight='bold',
 		family='Montserrat')
-legend_annotate('net_new_Commons_content_pages', 'Commons',wmf_colors['pink'])
-legend_annotate('net_new_Wikidata_entities', 'Wikidata',wmf_colors['brightgreen'])
-legend_annotate('net_new_Wikipedia_articles', 'Wikipedia',wmf_colors['purple'])
+#annotate in order of last-y (for padding)
+for i in range(3):
+	keyref = lastys.iloc[i].name
+	legend_annotate(key.loc[keyref,'colname'],
+		key.loc[keyref,'labelname'],
+		key.loc[keyref,'color'],
+		lastys.iloc[i].ypad)
+#legend_annotate('net_new_Commons_content_pages', 'Commons',wmf_colors['pink'])
+#legend_annotate('net_new_Wikidata_entities', 'Wikidata',wmf_colors['brightgreen'])
+#legend_annotate('net_new_Wikipedia_articles', 'Wikipedia',wmf_colors['purple'])
 
 #make YoY annotation
-def yoy_annotation(data_label,label_color):
+def yoy_annotation(data_label,label_color,ypad):
 	yoy_change_percent = ((yoy_highlight[data_label].iat[-1] - yoy_highlight[data_label].iat[0]) /  yoy_highlight[data_label].iat[0]) * 100
 	if yoy_change_percent > 0:
 		yoy_annotation = f"+{yoy_change_percent:.1f}% YoY"
@@ -178,20 +223,25 @@ def yoy_annotation(data_label,label_color):
 		yoy_annotation = f"{yoy_change_percent:.1f}% YoY"
 	plt.annotate(yoy_annotation,
 		xy = (yoy_highlight['month'].iat[-1],yoy_highlight[data_label].iat[-1]),
-		xytext = (100,-5),
+		xytext = (100,-5 + ypad),
 		xycoords = 'data',
 		textcoords = 'offset points',
-		color=label_color,
+		color='black',
 		family='Montserrat',
 		fontsize=14,
-		weight='bold',
-		bbox=dict(pad=0, facecolor="white", edgecolor="none"))
-yoy_annotation('net_new_Commons_content_pages',wmf_colors['pink'])
-yoy_annotation('net_new_Wikidata_entities',wmf_colors['brightgreen'])
-yoy_annotation('net_new_Wikipedia_articles',wmf_colors['purple'])
+		weight='bold')
+for i in range(3):
+	keyref = lastys.iloc[i].name
+	yoy_annotation(key.loc[keyref,'colname'],
+		key.loc[keyref,'color'],
+		lastys.iloc[i].ypad)
+#yoy_annotation('net_new_Commons_content_pages',wmf_colors['pink'])
+#yoy_annotation('net_new_Wikidata_entities',wmf_colors['brightgreen'])
+#yoy_annotation('net_new_Wikipedia_articles',wmf_colors['purple'])
 
 #data notes
-plt.figtext(0.1, 0.1, "Graph Notes: Created by Hua Xi 12/12/22 using data from https://github.com/wikimedia-research/Reader-movement-metrics", fontsize=8, family='Montserrat', color= wmf_colors['black25'])
+today = date.today()
+plt.figtext(0.1, 0.1, "Graph Notes: Created by Hua Xi " + str(today) + " using data from https://github.com/wikimedia-research/Reader-movement-metrics", fontsize=8, family='Montserrat', color= wmf_colors['black25'])
 
 #---SHOW GRAPH---
 #save as image
--- new_returning/new_returning.py
+++ new_returning/new_returning.py
@@ -5,6 +5,7 @@ import matplotlib.pyplot as plt
 import matplotlib.font_manager
 import numpy as np
 import re
+import calendar
 
 #---PROMPT FOR INPUT---
 outfile_name = input('Outfile_name:\n') or "New_Returning_Chart.png"
@@ -21,14 +22,19 @@ df = pd.read_csv('../data/editor_metrics.tsv', sep='\t')
 #print(df.active_editors.dtype)
 #print(df.month.dtype)
 
+start_date = "2019-01-01"
+end_date = "2023-01-01"
+month_interest = 1
+month_name = calendar.month_name[month_interest]
+
 #convert string to datetime
 df['month'] = pd.to_datetime(df['month'])
 
 #---BREAK DATA INTO SUBSETS--
 #only data since 2019
-df = df[df["month"].isin(pd.date_range("2019-01-01", "2022-12-01"))]
+df = df[df["month"].isin(pd.date_range(start_date, end_date))]
 #month of interest only
-monthly_df = df[df['month'].dt.month == 12]
+monthly_df = df[df['month'].dt.month == month_interest]
 #highlight the last two months
 yoy_highlight = pd.concat([df.iloc[-13,:],df.iloc[-1,:]],axis=1).T
 #highlighted_months = df[df['month'].isin(['2021-10-01','2022-10-01'])]
@@ -88,7 +94,7 @@ plt.scatter(yoy_highlight.month, yoy_highlight.new_active_editors,
 
 #---FORMATTING---
 #add title and labels
-plt.title('New and Returning Editors',font='Montserrat',weight='bold',fontsize=24,loc='left')
+plt.title(f'New and Returning Editors ({month_name})',font='Montserrat',weight='bold',fontsize=24,loc='left',pad=10)
 #plt.xlabel("Month",font='Montserrat', fontsize=18, labelpad=10) #source serif pro
 #plt.ylabel("Active Editors",font='Montserrat', fontsize=18)
 
@@ -106,7 +112,7 @@ plt.legend(frameon=False,
 '''
 
 #expand bottom margin
-plt.subplots_adjust(bottom=0.2, left=0.1, right=0.75)
+plt.subplots_adjust(bottom=0.1, left=0.1, right=0.75)
 
 #remove bounding box
 for pos in ['right', 'top', 'bottom', 'left']:
@@ -123,12 +129,21 @@ plt.gca().set_yticklabels([y_label_formatter(x) for x in current_values])
 plt.yticks(fontname = 'Montserrat',fontsize=14)
 plt.xticks(fontname = 'Montserrat',fontsize=14)
 
-
 #add monthly x-axis labels
+'''
 date_labels = []
 for dl in monthly_df['month']:
 	date_labels.append(datetime.datetime.strftime(dl, '%b %Y'))
 plt.xticks(ticks=monthly_df['month'],labels=date_labels,fontsize=14,fontname = 'Montserrat')
+'''
+
+#yearly x-axis labels on January
+date_labels = []
+date_labels_raw = pd.date_range(start_date, end_date, freq='AS-JAN')
+for dl in date_labels_raw:
+	date_labels.append(datetime.datetime.strftime(dl, '%Y'))
+plt.xticks(ticks=date_labels_raw,labels=date_labels)
+
 
 #---ADD ANNOTATIONS---
 #add combined annotation
@@ -157,8 +172,8 @@ def annotate(data_label, legend_label, label_color, x_distance):
 		weight='bold',
 		wrap=True,
 		family='Montserrat')
-annotate('new_active_editors', 'New',wmf_colors['green50'], 60)
-annotate('returning_active_editors', 'Returning',wmf_colors['blue'], 100)
+annotate('new_active_editors', 'New',wmf_colors['green50'], 55)
+annotate('returning_active_editors', 'Returning',wmf_colors['blue'], 95)
 
 '''
 #add legend as data labels
