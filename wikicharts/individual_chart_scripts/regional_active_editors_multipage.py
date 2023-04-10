79a3ecfdbc29ff9390604381097ab8f215782d26
cynthiaxhua <cynthiaxhua@gmail.com>
2023-04-10 14:30:43 -0400

    regional charts

--- wikicharts/individual_chart_scripts/regional_active_editors_multipage.py
+++ wikicharts/individual_chart_scripts/regional_active_editors_multipage.py
@@ -13,6 +13,7 @@ from wikicharts import Wikichart
 from wikicharts import wmf_colors
 from wikicharts import split_df_by_col
 from wikicharts import gen_keys
+from wikicharts import roll
 
 def main(argv):
 	print("Generating Regional Active Editors chart...")
@@ -63,99 +64,158 @@ def main(argv):
 	#print Totals row (sorted)
 	#delete Totals row
 	df = df.iloc[:-1]
+	df = df.rename(columns={'UNCLASSED':"Unclassed"})
+
+	#add code to set month as index
+	#df = df.set_index('month')
 
 	#divide into sets of four
 	dfs = split_df_by_col(df)
-	#print(dfs)
 
 	#generate keys that correspond each region to a diff color
 	key_colors = [wmf_colors['red'], wmf_colors['orange'], wmf_colors['yellow'], wmf_colors['green'], wmf_colors['purple'], wmf_colors['blue'], wmf_colors['pink'], wmf_colors['black50'], wmf_colors['brightblue']]
 	keys = gen_keys(dfs, key_colors)
 
-	'''
-	#---PREPARE TO PLOT
-	key1 = pd.DataFrame([['Central & Eastern Europe & Central Asia',wmf_colors['red']],
-		['East, Southeast Asia, & Pacific',wmf_colors['orange']],
-		['Latin America & Caribbean',wmf_colors['yellow']],
-		['Middle East & North Africa',wmf_colors['green']]],
-		index=['centraleuro','asiapacific','latamcarib','mideast'],
-		columns=['labelname','color'])
-
-	key2 = pd.DataFrame([['North America',wmf_colors['blue']],
-		['Northern & Western Europe',wmf_colors['purple']],
-		['South Asia',wmf_colors['pink']],
-		['Sub-Saharan Africa',wmf_colors['black50']]],
-		index=['northam','northeu','southasia','subafri'],
-		columns=['labelname','color'])
-	'''
-	
-	#---MAKE CHART---
-	'''
-	#first set of four
-	chart1 = Wikichart(start_date,end_date,dfs[0])
-	#print(dfs[0]['month'])
-	#plt.figure(1)
-	chart1.init_plot(width=12,subplotsx=2,subplotsy=2,fignum=0)
-	chart1.plot_subplots_lines('month', keys[0])
-	maxrange1 = chart1.get_maxrange()
-	#second set of four
-	chart2 = Wikichart(start_date,end_date,dfs[1])
-	chart2.init_plot(width=12,subplotsx=2,subplotsy=2,fignum=1)
-	chart2.plot_subplots_lines('month', keys[1])
-	maxrange2 = chart2.get_maxrange()
-	#calculate the largest range between the two figures and 8 subplots
-	maxrange = max(maxrange1,maxrange2)
-	#format and display figure 1
-	plt.figure(0)
-	chart1.standardize_subplotyrange(maxrange)
-	chart1.format_subplots(title = 'Regional Active Editors',
-		key = keys[0],
-		data_source="https://docs.google.com/spreadsheets/d/13XrrnCaz9qsKs5Gu_lUs2jtsK9VrSiGlilCleDgR6KM")
-	#save chart1 but set display to False because plt.show() will show all figures at once
-	chart1.finalize_plot(save_file_name1,display=False)
-	#format and display figure 2
-	plt.figure(1)
-	chart2.standardize_subplotyrange(maxrange)
-	chart2.format_subplots(title = 'Regional Active Editors',
-		key = keys[1],
-		data_source="https://docs.google.com/spreadsheets/d/13XrrnCaz9qsKs5Gu_lUs2jtsK9VrSiGlilCleDgR6KM")
-	chart2.finalize_plot(save_file_name2,display=display_flag)
-	'''
-	#print(dfs[2])
-	#print(dfs[2].dtypes)
-	#number of charts (subtracting 1 for the "month" column)
+	#---MAKE CHARTS---
+	fig_counter = 0
 	total_num_charts = len(df.columns) - 1
 	charts_per_figure = 4
 	num_figures = ceil(total_num_charts / charts_per_figure)
-	charts = [None]*num_figures
+	figures = [None]*num_figures
 	#max range across figures
 	maxranges = [None]*num_figures 
 	num_ticks = [None]*num_figures
 	#initialize each figure
 	for f in range(num_figures):
 		figure_num_charts = len(dfs[f].columns) - 1
-		charts[f] = Wikichart(start_date,end_date,dfs[f])
-		charts[f].init_plot(width=12,subplotsx=2,subplotsy=2,fignum=f)
-		charts[f].plot_subplots_lines('month', keys[f], num_charts=figure_num_charts)
-		maxranges[f], num_ticks[f] = charts[f].get_maxyrange()
+		figures[f] = Wikichart(start_date,end_date,dfs[f])
+		figures[f].init_plot(width=12,subplotsx=2,subplotsy=2,fignum=f)
+		figures[f].plot_subplots_lines('month', keys[f], num_charts=figure_num_charts)
+		maxranges[f], num_ticks[f] = figures[f].get_maxyrange()
 	#calculate the largest range between the two figures and 8 subplots
 	maxrange = max(maxranges)
 	maxrange_index = maxranges.index(maxrange)
 	maxrange_numticks = num_ticks[maxrange_index]
-	#format and display each figure
+	#format and display each figure in multi-chart figures
 	for f in range(num_figures):
 		plt.figure(f)
 		figure_num_charts = len(dfs[f].columns) - 1
-		charts[f].standardize_subplotyrange(maxrange, maxrange_numticks, num_charts=figure_num_charts)
-		charts[f].format_subplots(title = 'Regional Active Editors',
+		figures[f].standardize_subplotyrange(maxrange, maxrange_numticks, num_charts=figure_num_charts)
+		figures[f].format_subplots(title = 'Regional Active Editors',
 			key = keys[f],
 			data_source="https://docs.google.com/spreadsheets/d/13XrrnCaz9qsKs5Gu_lUs2jtsK9VrSiGlilCleDgR6KM",
 			num_charts=figure_num_charts)
+		figures[f].clean_ylabels_subplots()
 		#save chart1 but set display to False because plt.show() will show all figures at once
 		save_file_name = dirname(script_directory) + "/charts/" + outfile_name + "_" + str(f) + ".jpeg"
-		charts[f].finalize_plot(save_file_name,display=False)
+		figures[f].finalize_plot(save_file_name,display=False)
 	plt.show()
-
+	fig_counter = num_figures
+	
+	'''
+	#---GENERATE INDIVIDUAL CHARTS---
+	individual_charts = [None]*total_num_charts
+	columns = list(df.columns)
+	columns.remove('month')
+	for c in range(len(columns)):
+		fig_counter = fig_counter + 1
+		current_col = columns[c]
+		current_df = df[['month', current_col]]
+		current_savefile = dirname(script_directory) + "/charts/individual_" + outfile_name + "_" + str(c) + ".jpeg"
+		individual_charts[c] = Wikichart(start_date,end_date,current_df)
+		individual_charts[c].init_plot(fignum=fig_counter)
+		individual_charts[c].plot_line('month',current_col,key_colors[c])
+		individual_charts[c].plot_line('month',current_col,key_colors[c])
+		individual_charts[c].plot_monthlyscatter('month',current_col,col =key_colors[c])
+		individual_charts[c].plot_yoy_highlight('month',current_col)
+		individual_charts[c].standardize_yrange(maxrange, maxrange_numticks)
+		individual_charts[c].format(title = f'Active Editors: {current_col}',
+			ybuffer=False,
+			data_source="https://docs.google.com/spreadsheets/d/13XrrnCaz9qsKs5Gu_lUs2jtsK9VrSiGlilCleDgR6KM",
+			tadjust=0.825,badjust=0.125,
+			titlepad=25)
+		individual_charts[c].annotate(x='month',
+			y=current_col,
+			num_annotation=individual_charts[c].calc_yoy(y=current_col,yoy_note=yoy_note))
+		individual_charts[c].finalize_plot(current_savefile,display=False)
+	plt.show()
+	'''
+	#---AGGREGATE VIEWS---
+	#Eight Figure View
+	charts_per_figure = 8
+	keys = gen_keys([df], key_colors)
+	fig_counter += 1
+	main_fig = Wikichart(start_date,end_date,df)
+	main_fig.init_plot(width=12,subplotsx=2,subplotsy=4,fignum=fig_counter)
+	main_fig.plot_subplots_lines(x='month', key=keys[0], num_charts=charts_per_figure, subplot_title_size=9)
+	main_fig.standardize_subplotyrange(maxrange, maxrange_numticks, num_charts=charts_per_figure)
+	main_fig.format_subplots(title = 'Regional Active Editors',
+			key = keys[0],
+			data_source="https://docs.google.com/spreadsheets/d/13XrrnCaz9qsKs5Gu_lUs2jtsK9VrSiGlilCleDgR6KM",
+			num_charts=charts_per_figure,
+			tickfontsize=8)
+	main_fig.clean_ylabels_subplots(tickfontsize=8)
+	save_file_name = dirname(script_directory) + "/charts/" + outfile_name + "_fullview.jpeg"
+	main_fig.finalize_plot(save_file_name,display=False)
+	plt.show()
+	#Rolling View
+	core_df = df.drop(columns=['Unclassed'])
+	rolling = roll(core_df, rolling_months = 3)
+	fig_counter += 1
+	main_fig = Wikichart(start_date,end_date,rolling)
+	main_fig.init_plot(width=12,subplotsx=2,subplotsy=4,fignum=fig_counter)
+	main_fig.plot_subplots_lines(x='month', key=keys[0], num_charts=charts_per_figure, subplot_title_size=9)
+	main_fig.standardize_subplotyrange(maxrange, maxrange_numticks, num_charts=charts_per_figure)
+	main_fig.format_subplots(title = 'Regional Active Editors: Rolling',
+			key = keys[0],
+			data_source="https://docs.google.com/spreadsheets/d/13XrrnCaz9qsKs5Gu_lUs2jtsK9VrSiGlilCleDgR6KM",
+			num_charts=charts_per_figure,
+			tickfontsize=8,
+			mo_in_title=False)
+	main_fig.clean_ylabels_subplots(tickfontsize=8)
+	save_file_name = dirname(script_directory) + "/charts/" + outfile_name + "_rolling.jpeg"
+	main_fig.finalize_plot(save_file_name,display=False)
+	plt.show()
+	#Annual
+	core_df = df.drop(columns=['Unclassed'])
+	annual = core_df.groupby(core_df.month.dt.year).mean().reset_index()
+	fig_counter += 1
+	main_fig = Wikichart(start_date,end_date,annual)
+	main_fig.init_plot(width=12,subplotsx=2,subplotsy=4,fignum=fig_counter)
+	main_fig.plot_subplots_lines(x='month', key=keys[0], num_charts=charts_per_figure, subplot_title_size=9)
+	main_fig.standardize_subplotyrange(maxrange, maxrange_numticks, num_charts=charts_per_figure)
+	main_fig.format_subplots(title = 'Regional Active Editors: Annual Average',
+			key = keys[0],
+			data_source="https://docs.google.com/spreadsheets/d/13XrrnCaz9qsKs5Gu_lUs2jtsK9VrSiGlilCleDgR6KM",
+			num_charts=charts_per_figure,
+			tickfontsize=8,
+			mo_in_title=False)
+	main_fig.clean_ylabels_subplots(tickfontsize=8)
+	save_file_name = dirname(script_directory) + "/charts/" + outfile_name + "_annual.jpeg"
+	main_fig.finalize_plot(save_file_name,display=False)
+	plt.show()
+	#Quarterly
+	core_df = df.drop(columns=['Unclassed'])
+	quarterly = core_df.groupby(core_df['month'].dt.to_period('Q')).mean().reset_index()
+	print(quarterly)
+	quarterly['month'] = quarterly['month'].astype(str)
+	quarterly['month'] = pd.to_datetime(quarterly['month'])
+	print(quarterly)
+	fig_counter += 1
+	main_fig = Wikichart(start_date,end_date,quarterly)
+	main_fig.init_plot(width=12,subplotsx=2,subplotsy=4,fignum=fig_counter)
+	main_fig.plot_subplots_lines(x='month', key=keys[0], num_charts=charts_per_figure, subplot_title_size=9)
+	main_fig.standardize_subplotyrange(maxrange, maxrange_numticks, num_charts=charts_per_figure)
+	main_fig.format_subplots(title = 'Regional Active Editors: Quarterly Average',
+			key = keys[0],
+			data_source="https://docs.google.com/spreadsheets/d/13XrrnCaz9qsKs5Gu_lUs2jtsK9VrSiGlilCleDgR6KM",
+			num_charts=charts_per_figure,
+			tickfontsize=8,
+			mo_in_title=False)
+	main_fig.clean_ylabels_subplots(tickfontsize=8)
+	save_file_name = dirname(script_directory) + "/charts/" + outfile_name + "_quarterly.jpeg"
+	main_fig.finalize_plot(save_file_name,display=False)
+	plt.show()
 
 if __name__ == "__main__":
 	main(sys.argv[1:])
--- wikicharts/wikicharts.py
+++ wikicharts/wikicharts.py
@@ -112,6 +112,11 @@ def closestdivisible(n, m) :
     # else n2 is the required closest number
     return n2
 
+#take a df and create a parallel df of rolling averages
+def roll(df, rolling_months = 3, index_column_name = "month"):
+	rolled = df.set_index('month').rolling(rolling_months).mean().reset_index().dropna()
+	return rolled
+
 #---BASIC CHART---
 #the wrapper's main functionality is in the formatting and annotation
 #the plotting functions could actually probably be deleted bc they just repeat matplotlib's functions
@@ -158,13 +163,12 @@ class Wikichart:
 			zorder=4)
 			#note: due to a bug in matplotlib, the grid's zorder is fixed at 2.5 so everything plotted must be above 2.5
 	
-	def plot_yoy_highlight(self, x, y, col = wmf_colors['yellow'], legend_label ='_nolegend_'):
+	def plot_yoy_highlight(self, x, y, highlight_radius = 1000, col = wmf_colors['yellow'], legend_label ='_nolegend_'):
 		yoy_highlight = pd.concat([self.df.iloc[-13,:],self.df.iloc[-1,:]],axis=1).T
 		#dots on month of interest
-		highlight_radius = 1000000
 		plt.scatter(yoy_highlight[str(x)], yoy_highlight[str(y)],
 			label=legend_label,
-			s=(highlight_radius**0.5),
+			s=highlight_radius,
 			facecolors='none',
 			edgecolors=col,
 			zorder=5)
@@ -177,7 +181,7 @@ class Wikichart:
 			edgecolor=col,
 			zorder=3)
 
-	def plot_subplots_lines(self, x, key, linewidth=2, num_charts=4):
+	def plot_subplots_lines(self, x, key, linewidth=2, num_charts=4, subplot_title_size = 12):
 		#remove bounding box
 		i = 0
 		for row in self.ax:
@@ -191,34 +195,35 @@ class Wikichart:
 						color=region_color,
 						zorder=3,
 						linewidth=linewidth)
-					axis.set_title(region_label,fontfamily=style_parameters['font'],fontsize=12)
+					axis.set_title(region_label,fontfamily=style_parameters['font'],fontsize=subplot_title_size)
 				i += 1
 
-	def format(self, title, author=parameters['author'], data_source="N/A",radjust=0.85,ladjust=0.1,tadjust=0.9,badjust=0.1):
+	def format(self, title, author=parameters['author'], data_source="N/A",ybuffer=True,radjust=0.85,ladjust=0.1,tadjust=0.9,badjust=0.1,titlepad=0):
 		#add gridlines
 		plt.grid(axis = 'y', zorder=-1, color = wmf_colors['black25'], linewidth = 0.25)
 		#format title
 		custom_title = f'{title} ({calendar.month_name[self.month_interest]})'
-		plt.title(custom_title,font=style_parameters['font'],fontsize=style_parameters['title_font_size'],weight='bold',loc='left')
+		plt.title(custom_title,font=style_parameters['font'],fontsize=style_parameters['title_font_size'],weight='bold',loc='left',wrap=True,pad=titlepad)
 		#remove bounding box
 		for pos in ['right', 'top', 'bottom', 'left']:
 			plt.gca().spines[pos].set_visible(False)
 		#expand bottom margin (to make room for author and data source annotation)
 		plt.subplots_adjust(bottom=badjust, right = radjust, left=ladjust, top=tadjust)
-		#format y-axis range (gca = get current axis)
-		#we want the plotted portion to be 2/3rds the total y axis range
-		ax = plt.gca()
-		current_ylim = ax.get_ylim()
-		current_yrange = current_ylim[1] - current_ylim[0]
-		new_ymin = current_ylim[0] - current_yrange / 4
-		new_ymax = current_ylim[1] + current_yrange / 4
-		ax.set_ylim([new_ymin, new_ymax])
+		#buffer y-axis range to be 2/3rds of the total y axis range
+		#note: gca = get current axis
+		if ybuffer == True:
+			ax = plt.gca()
+			current_ylim = ax.get_ylim()
+			current_yrange = current_ylim[1] - current_ylim[0]
+			new_ymin = current_ylim[0] - current_yrange / 4
+			new_ymax = current_ylim[1] + current_yrange / 4
+			ax.set_ylim([new_ymin, new_ymax])
 		#format x-axis labels — yearly x-axis labels on January
 		plt.xticks(fontname=style_parameters['font'],fontsize=style_parameters['text_font_size'])
 		date_labels = []
 		date_labels_raw = pd.date_range(self.start_date, self.end_date, freq='AS-JAN')
 		for dl in date_labels_raw:
-			date_labels.append(datetime.datetime.strftime(dl, '%Y'))
+			date_labels.append(datetime.strftime(dl, '%Y'))
 		plt.xticks(ticks=date_labels_raw,labels=date_labels)
 		#format y-axis labels
 		warnings.filterwarnings("ignore")
@@ -234,7 +239,7 @@ class Wikichart:
 		today = date.today()
 		plt.figtext(0.1, 0.025, "Graph Notes: Created by " + str(author) + " " + str(today) + " using data from " + str(data_source), family=style_parameters['font'],fontsize=8, color= wmf_colors['black25'])
 
-	def format_subplots(self, title, key, author=parameters['author'], data_source="N/A", radjust=0.85, ladjust=0.1,tadjust=0.85,badjust=0.1, num_charts=4):
+	def format_subplots(self, title, key, author=parameters['author'], data_source="N/A", radjust=0.85, ladjust=0.1,tadjust=0.85,badjust=0.1, num_charts=4,tickfontsize=12,mo_in_title=True):
 		#expand bottom margin
 		plt.subplots_adjust(bottom=badjust, right = radjust, left=ladjust, top=tadjust, wspace=0.2, hspace=0.4)
 		#count number of charts and stop when num_charts is hit
@@ -247,15 +252,10 @@ class Wikichart:
 					#gridlines
 					axis.grid(axis = 'y', zorder=-1, color = wmf_colors['black25'], linewidth = 0.25)
 					#format x axis labels
-					axis.set_xticklabels(axis.get_xticklabels(),fontfamily=style_parameters['font'])
-					'''
-					date_labels = []
-					date_labels_raw = pd.date_range(self.start_date, self.end_date, freq='AS-JAN')
-					for dl in date_labels_raw:
-						date_labels.append(datetime.strftime(dl, '%Y'))
-					axis.set_xticklabels(date_labels_raw)
-					axis.set_xticklabels(date_labels)
-					'''
+					axis.set_xticklabels(axis.get_xticklabels(),fontfamily=style_parameters['font'],fontsize=tickfontsize)
+					#format x axis labels to show year only
+					xaxisFormatter = mdates.DateFormatter('%Y')
+					axis.xaxis.set_major_formatter(xaxisFormatter)
 					#format y axis labels
 					current_values = axis.get_yticklabels()
 					new_labels = []
@@ -271,18 +271,35 @@ class Wikichart:
 					if new_labels[0] == str(0):
 						new_labels[0] = ""
 					'''
-					axis.set_yticklabels(new_labels,fontfamily=style_parameters['font'])
+					axis.set_yticklabels(new_labels,fontfamily=style_parameters['font'],fontsize=tickfontsize)
 				else:
 					#make invisible if outside of num_charts
 					axis.set_visible(False)
 				i += 1
 		#add title and axis labels
 		#note there seems to be a bug with ha and va args to suptitle, so just set x and y manually
-		self.fig.suptitle(f'{title} ({calendar.month_name[self.month_interest]})',ha='left',x=0.05,y=0.97,fontsize=style_parameters['title_font_size'],fontproperties={'family':style_parameters['font'],'weight':'bold'})
+		if mo_in_title:
+			figure_title = f'{title} ({calendar.month_name[self.month_interest]})'
+		else:
+			figure_title = f'{title}'
+		self.fig.suptitle(figure_title,ha='left',x=0.05,y=0.97,fontsize=style_parameters['title_font_size'],fontproperties={'family':style_parameters['font'],'weight':'bold'})
 		#add bottom annotation
 		today = date.today()
 		plt.figtext(0.05,0.01, "Graph Notes: Created by " + str(author) + " " + str(today) + " using data from " + str(data_source), fontsize=8, va="bottom", ha="left", color=wmf_colors['black25'], fontproperties={'family':style_parameters['font']})
 	
+	#show only bottom and top ylabel
+	def clean_ylabels_subplots(self,tickfontsize=12):
+		#count number of charts and stop when num_charts is hit
+		i = 0
+		for row in self.ax:
+			for axis in row:
+				current_labels = axis.get_yticklabels()
+				new_labels = ['']*len(current_labels)
+				new_labels[0] = current_labels[0]
+				new_labels[-1] = current_labels[-1]
+				axis.set_yticklabels(new_labels,fontfamily=style_parameters['font'],fontsize=tickfontsize)
+				i += 1	
+
 	#set every subplot to the same ymin and ymax
 	def standardize_subplotyaxis(self, ymin, ymax, num_charts=4):
 		i = 0
@@ -292,8 +309,35 @@ class Wikichart:
 					axis.set_ylim([ymin, ymax])
 				i += 1
 
+	#set yrange for a single chart plot
+	def standardize_yrange(self, yrange, num_ticks,std_cutoff=15):
+		#get the standard y interval
+		std_yinterval = yrange / (num_ticks - 1)
+		ax = plt.gca()
+		current_ymin, current_ymax = ax.get_ylim()
+		current_yrange = current_ymax - current_ymin
+		if current_yrange > (yrange / std_cutoff):
+			current_ymedian = current_ymin + ((current_ymax - current_ymin) / 2)
+			if (num_ticks % 2) == 0:
+				#for final even num ticks
+				new_ymedian = closestdivisible(current_ymedian, std_yinterval)
+				if new_ymedian < current_ymedian:
+					new_ymin = new_ymedian - (std_yinterval * (num_ticks / 2 - 1))
+				else:
+					new_ymin = new_ymedian - (std_yinterval * (num_ticks / 2))
+			else:
+				#for final odd number of ticks 
+				new_ymedian = closestdivisible(current_ymedian, std_yinterval)
+				new_ymin = new_ymedian - (yrange / 2)
+			#set min to 0 if negative
+			new_ymin = max(0, new_ymin)
+			#set ymax
+			new_ymax = new_ymin + yrange
+			ax.set_ylim(new_ymin, new_ymax)
+
 	#set every subplot to the same yrange and intervals
-	def standardize_subplotyrange(self, yrange, num_ticks, yspacing = 50000000, num_charts=4):
+	#std_cutoff is a number where if the current yrange < (std_cutoff x std_yrange), then we don't standardize it; used to keep natural range for very small ranged plots
+	def standardize_subplotyrange(self, yrange, num_ticks, num_charts=4, std_cutoff=15):
 		#get the standard y interval
 		std_yinterval = yrange / (num_ticks - 1)
 		#keep track of chart num
@@ -304,27 +348,33 @@ class Wikichart:
 				if i < num_charts:
 					#get current y range
 					current_ymin, current_ymax = axis.get_ylim()
-					current_ymedian = current_ymin + ((current_ymax - current_ymin) / 2)
-					if (num_ticks % 2) == 0:
-						#for final even num ticks
-						new_ymedian = closestdivisible(current_ymedian, std_yinterval)
-						print(num_ticks)
-						if new_ymedian < current_ymedian:
-							new_ymin = new_ymedian - (std_yinterval * (num_ticks / 2 - 1))
+					current_yrange = current_ymax - current_ymin
+					if current_yrange > (yrange / std_cutoff):
+						current_ymedian = current_ymin + ((current_ymax - current_ymin) / 2)
+						if (num_ticks % 2) == 0:
+							#for final even num ticks
+							new_ymedian = closestdivisible(current_ymedian, std_yinterval)
+							if new_ymedian < current_ymedian:
+								new_ymin = new_ymedian - (std_yinterval * (num_ticks / 2 - 1))
+							else:
+								new_ymin = new_ymedian - (std_yinterval * (num_ticks / 2))
 						else:
-							new_ymin = new_ymedian - (std_yinterval * (num_ticks / 2))
-					else:
-						#for final odd number of ticks 
-						new_ymedian = closestdivisible(current_ymedian, std_yinterval)
-						new_ymin = new_ymedian - (yrange / 2)
-					#set min to 0 if negative
-					new_ymin = max(0, new_ymin)
-					#set ymax
-					new_ymax = new_ymin + yrange
-					axis.set_ylim(new_ymin, new_ymax)
-					#axis.yaxis.set_major_locator(ticker.MultipleLocator(yspacing))
-					#axis.Axis.set_minor_locator(minorLocator)
+							#for final odd number of ticks 
+							new_ymedian = closestdivisible(current_ymedian, std_yinterval)
+							new_ymin = new_ymedian - (yrange / 2)
+						#set min to 0 if negative
+						new_ymin = max(0, new_ymin)
+						#set ymax
+						new_ymax = new_ymin + yrange
+						axis.set_ylim(new_ymin, new_ymax)
+						#axis.yaxis.set_major_locator(ticker.MultipleLocator(yspacing))
+						#axis.Axis.set_minor_locator(minorLocator)
 				i += 1
+
+	def get_ytickrange(self):
+		ticks = self.ax.get_yticklabels()
+		tick_range = ticks[-1].get_position()[1] - ticks[0].get_position()[1]
+		return tick_range
 
 	#returns the subplot with the max range, and its corresponding number of ticks
 	#need to change to getting max tick range
@@ -336,7 +386,6 @@ class Wikichart:
 				#gets the tick range
 				ticks = axis.get_yticklabels()
 				tick_range = ticks[-1].get_position()[1] - ticks[0].get_position()[1]
-				print("expanded tick range " + str(tick_range))
 				self.yranges.append(tick_range)
 				#get tick intervals
 				self.ynumticks.append(len(ticks))
@@ -368,7 +417,9 @@ class Wikichart:
 	def calc_yoy(self,y,yoy_note=""):
 		yoy_highlight = pd.concat([self.df.iloc[-13,:],self.df.iloc[-1,:]],axis=1).T
 		yoy_change_percent = ((yoy_highlight[str(y)].iat[-1] - yoy_highlight[str(y)].iat[0]) /  yoy_highlight[str(y)].iat[0]) * 100
-		if yoy_change_percent > 0:
+		if math.isnan(yoy_change_percent):
+			yoy_annotation = "YoY N/A"
+		elif yoy_change_percent > 0:
 			yoy_annotation = f" +{yoy_change_percent:.1f}% YoY" + " " + yoy_note
 		else:
 			yoy_annotation = f" {yoy_change_percent:.1f}% YoY" + " " + yoy_note
@@ -465,18 +516,31 @@ class Wikichart:
 		block_height = ymax - ymin
 
 		#plot rectangle
+		matplotlib.rcParams['hatch.linewidth'] = 0.25  # previous pdf hatch linewidth
 		rect = Rectangle((xstart - xbuffer, ymin), block_width + 2 * xbuffer, block_height, 
-			color='white', 
-			linewidth=0,
-			fill='white',
-			edgecolor=None,
+			linewidth=0, #no edge around rectangle
+			hatch='////',
+			edgecolor=wmf_colors['black75'], #hatch color
+			facecolor='white', #bg color
 			zorder=10)
 		axis.add_patch(rect) 
 
-	def block_off_multi(self,blockstart, blockend, xbuffer = 7):
+	def block_off_multi(self,blockstart, blockend, xbuffer = 6):
 		for row in self.ax:
 			for axis in row:
 				self.block_off(axis,blockstart, blockend, xbuffer)
+
+		#draws a rectangle to block off a set of dates
+	def add_block_legend(self):
+		self.fig.patches.extend([plt.Rectangle((0.05, 0.868), 0.01, 0.02,
+			linewidth=0.1, #no edge around rectangle
+			hatch='//////',
+			edgecolor='black', #hatch color
+			facecolor='white', #bg color
+			zorder=100,
+			transform=self.fig.transFigure,
+			figure = self.fig)])
+		#self.ax[0][1].add_patch(rect) 
 
 
 
