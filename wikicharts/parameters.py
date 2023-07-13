ed9ae838521e4a7ff0e85e33406a95dffb0b106d
Neil Shah-Quinn <nshahquinn@wikimedia.org>
2023-07-13 02:47:04 +0000

    Parameterize and clean up reading of metrics.tsv files

--- wikicharts/parameters.py
+++ wikicharts/parameters.py
@@ -1,3 +1,4 @@
-data_path = "../../Editing-movement-metrics/metrics/metrics.tsv"
-save_path = "../../Editing-movement-metrics/charts/"
+editing_data_path = "../../Editing-movement-metrics/metrics/metrics.tsv"
+readers_data_path = "../../Readers-movement-metrics/metrics/metrics.tsv"
+save_directory = "../../Editing-movement-metrics/charts/"
 author = "Hua Xi"
