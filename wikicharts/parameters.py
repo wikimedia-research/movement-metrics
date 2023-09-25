4b5831c863b4b91fb32b2989e010928705bc0cc3
Mayakpwiki <Mayakp.wiki@gmail.com>
2023-09-25 23:26:04 +0000

    Unique devices regional breakdown changes

--- wikicharts/parameters.py
+++ wikicharts/parameters.py
@@ -1,4 +1,5 @@
 editing_data_path = "../../Editing-movement-metrics/metrics/metrics.tsv"
 readers_data_path = "../../Readers-movement-metrics/metrics/metrics.tsv"
-save_directory = "../../Editing-movement-metrics/charts/"
-author = "Hua Xi"
+unique_devices_data_path = "../../Readers-movement-metrics/metrics/metrics-unique-devices.tsv"
+save_directory = "../../Readers-movement-metrics/charts/"
+author = "Maya Kampurath"
