503c32b6c4ade764bdc7630d7b460ecb233a9c3d
Neil Shah-Quinn <nshahquinn@wikimedia.org>
2023-10-11 03:43:58 +0000

    Improve reporting code
    * Better handle missing data
    * Centralize report formatting code
    * Display only core metrics on quarterly report

--- src/utils.py
+++ src/utils.py
@@ -1,6 +1,7 @@
 from numbers import Number
 from pathlib import Path
 
+import numpy as np
 import pandas as pd
 
 from wmfdata.utils import pct_str, sig_figs
@@ -25,8 +26,52 @@ def load_all_metric_files():
     metrics = dfs[0].join(dfs[1:], how="outer", sort=True)
 
     return metrics
+    
+def subtract_year(period):
+    # As of Oct 2023, Pandas doesn't have a way to subtract
+    # a year from an arbitrary period (we want to support both months
+    # and quarters), so we have to cast to a timestamp, subtract,
+    # and cast back to a period
+    freq = period.freqstr
+    year_ago = period.to_timestamp() - pd.DateOffset(years=1)
+    return pd.Period(year_ago, freq=freq)
 
-def fmt_num(x):
+def calc_rpt(metric, reporting_period):
+    """
+    * metric: a Pandas time series giving the values of a metric
+    * reporting_period: a Pandas period object indicating the
+      period we want to report about.
+      
+    Returns a Pandas series containing measurements of the metric (currently,
+    the value during the reporting period, the year-over-year change, and a naive
+    forecast for the next period.
+    """
+    # Use get rather than direct indexing so that missing data results in NaNs rather
+    # than errors. We want NaNs, not nulls, so that math on them results in NaNs rather
+    # than errors.
+    value = metric.get(reporting_period, np.nan)
+
+    year_ago = subtract_year(reporting_period)
+    year_ago_value = metric.get(year_ago, np.nan)
+    
+    period_after_year_ago = year_ago + 1
+    period_after_year_ago_value = metric.get(period_after_year_ago, np.nan)
+    
+    if year_ago_value != 0:
+        year_over_year_change = (value / year_ago_value) - 1
+        naive_forecast = period_after_year_ago_value / year_ago_value * value
+    else:
+        year_over_year_change = np.nan
+        naive_forecast = np.nan
+    
+    return pd.Series({
+        "value": value,
+        "year_over_year_change": year_over_year_change,
+        "naive_forecast": naive_forecast
+    })
+
+
+def format_number(x):
     if isinstance(x, Number) and not pd.isnull(x):
         x = sig_figs(x, 3)
         M = 1_000_000
@@ -46,37 +91,33 @@ def fmt_num(x):
             return f"{x_in_G} B"
     else:
         return x
-    
-def subtract_year(period):
-    # As of Oct 2023, Pandas doesn't have a way to subtract
-    # a year from an arbitrary period (we want to support both months
-    # and quarters), so we have to cast to a timestamp, subtract,
-    # and cast back to a period
-    freq = period.freqstr
-    year_ago = period.to_timestamp() - pd.DateOffset(years=1)
-    return pd.Period(year_ago, freq=freq)
 
-def calc_rpt(ser):   
-    latest = ser.index[-1]
-    latest_value = ser[latest]
-    # previous_period = latest_period - 1
-    # previous_value = ser[previous_period]
+def format_report(df, metrics_type, reporting_period):
+    """
+    * df: a Pandas data frame (intended to be the output of applying calc_rpt to 
+      a data frame of metrics)
+    * metrics_type: a string identifying the type of metrics in the report
+      (e.g. "core", "essential"). Used to add a table header.
+    * reporting_period: a Pandas period identifying the period the report is 
+      about. Also used in the table header
+      
+    Returns a formatted and styled data frame useful for
+    human reading
+    """
+    header = f"{reporting_period} {metrics_type} metrics"
     
-    year_ago = subtract_year(latest)
-    year_ago_value = ser[year_ago]
-    period_after_year_ago = year_ago + 1
-    period_after_year_ago_value = ser[period_after_year_ago]
+    new_columns= pd.MultiIndex.from_product([[header], df.columns])
+    df.columns = new_columns
     
-    year_over_year_change = (latest_value / year_ago_value) - 1
-
-    naive_forecast = period_after_year_ago_value / year_ago_value * latest_value
-    
-    res = [latest_value, year_over_year_change, naive_forecast]
-    return pd.Series(
-        [fmt_num(n) for n in res],
-        index=[
-            "value",
-            "change_over_past_year",
-            "naive_forecast_for_next_period"
-        ]
+    df = (
+        df
+        .map(format_number)
+        .fillna("–")
+        .style
+        .set_table_styles([{
+            "selector": "th.col_heading.level0",
+            "props": "font-size: 1.5em; text-align: center; font-weight: bold;"
+        }])
     )
+    
+    return df
