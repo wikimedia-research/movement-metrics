45a55ecbe380929d430bf0ed4d3f201085638955
hghani-ctr <145786695+hghani-ctr@users.noreply.github.com>
2023-11-27 09:49:40 -0500

    Updated content gap metrics 2023
    Updated content gap metrics October 2023 
    -updated code for content gap plots

--- wikicharts/parameters.py
+++ wikicharts/parameters.py
@@ -1,5 +1,6 @@
 editing_data_path = "../metrics/editing_metrics.tsv"
 readers_data_path = "../metrics/readers_metrics.tsv"
 unique_devices_data_path = "../metrics/regional_unique_devices.tsv"
+content_gap_data_path = "../metrics/content_gap_data_metrics.tsv"
 save_directory = "../charts/"
 author = "Movement Insights"
