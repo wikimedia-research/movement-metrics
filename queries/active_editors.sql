e5e99ecd11597d7a2f537fb243b0ad79e6d8d7f2
Hamid Ghani <hghani@stat1008.eqiad.wmnet>
2024-03-07 16:53:53 +0000

    Updated query: T358684

--- queries/active_editors.sql
+++ queries/active_editors.sql
@@ -1,26 +1,43 @@
+WITH global_user_registration AS (
+    SELECT
+        user_name,
+        MIN(CAST(TRUNC(user_registration, 'MONTH') AS DATE)) AS global_registration_month
+    FROM wmf_product.editor_month
+    WHERE
+        user_id != 0
+        AND NOT bot_by_group
+        AND user_name NOT REGEXP 'bot\\b'
+    GROUP BY
+        user_name
+),
+aggregated_edits AS (
+    SELECT
+        e.month,
+        e.user_name,
+        SUM(e.content_edits) AS content_edits,
+        g.global_registration_month
+    FROM wmf_product.editor_month e
+    INNER JOIN global_user_registration g ON e.user_name = g.user_name
+    WHERE
+        e.MONTH = '{metrics_month_first_day}'
+        AND e.user_id != 0
+        AND NOT e.bot_by_group
+        AND e.user_name NOT REGEXP 'bot\\b'
+    GROUP BY
+        e.month,
+        e.user_name,
+        g.global_registration_month
+)
 SELECT
     month,
     COUNT(*) AS active_editors,
-    SUM(CAST(registration_month = month AS INT)) AS new_active_editors,
-    COUNT(*) - SUM(CAST(registration_month = month AS INT)) AS returning_active_editors
+    SUM(CAST(global_registration_month = CAST(month AS DATE) AS INT)) AS new_active_editors,
+    COUNT(*) - SUM(CAST(global_registration_month = CAST(month AS DATE) AS INT)) AS returning_active_editors
 FROM
-    (
-        SELECT
-            CAST(month AS DATE) AS month,
-            user_name,
-            SUM(content_edits) AS content_edits,
-            MAX(bot_by_group) AS bot_by_group,
-            CAST(TRUNC(MIN(user_registration), 'MONTH') AS DATE) AS registration_month
-        FROM wmf_product.editor_month
-        WHERE
-            MONTH = '{metrics_month_first_day}'
-            AND user_id != 0
-        GROUP BY
-            month,
-            user_name
-    ) global_edits
+    aggregated_edits
 WHERE
     content_edits >= 5
-    AND NOT bot_by_group
-    AND user_name NOT REGEXP 'bot\\b'
+    AND global_registration_month  = '{metrics_month_first_day}'
 GROUP BY month
+
+
